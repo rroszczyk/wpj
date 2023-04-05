b82f2d0eeb5165f520c6c00e0d3f6a20d78c3d55
Radosław Roszczyk <rroszczyk@users.noreply.github.com>
2023-04-05 12:12:17 +0200

    Object Pool - add package

---
+++ Object Pool/src/net/roszczyk/pool/ExportingProcess.java
@@ -0,0 +1,17 @@
+package net.roszczyk.pool;
+
+public class ExportingProcess {
+    private final long processNo;
+
+    public ExportingProcess(long processNo) {
+        this.processNo = processNo;
+
+        // TU MIESZKAJĄ SMOKI
+
+        System.out.println("Object with process no. " + processNo + " was created");
+    }
+
+    public long getProcessNo() {
+        return processNo;
+    }
+}
--- Object Pool/src/net/roszczyk/pool/Program.java
+++ Object Pool/src/net/roszczyk/pool/Program.java
@@ -1,3 +1,5 @@
+package net.roszczyk.pool;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
