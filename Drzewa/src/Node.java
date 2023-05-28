67793bddac7731a2c5a8eb80b6dd3b4a04aabeef
Radoslaw Roszczyk <rroszczyk@users.noreply.github.com>
2023-05-28 10:06:28 +0200

    Add files via upload

---
+++ Drzewa/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    public int data;
+    public Node leftChild;
+    public Node rightChild;
+
+    public Node(){}
+
+    public void display(){
+        System.out.print("("+data+ ")");
+    }
+}
